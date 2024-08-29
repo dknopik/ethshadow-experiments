822758ead485a79796f94bace2069a1a87779baf
Daniel Knopik <107140945+dknopik@users.noreply.github.com>
2024-08-29 22:13:11 +0200

    fix archive file ending

--- das_params/run.sh
+++ das_params/run.sh
@@ -2,7 +2,7 @@
 
 run() {
     bin/ethshadow -d data.$1 $1.yaml >> $1.out 2>>$1.err && \
-        tar -cS -I 'zstd --long -T0' -f $1.tar.xst data.$1 $1.out $1.err && \
+        tar -cS -I 'zstd --long -T0' -f $1.tar.zst data.$1 $1.out $1.err && \
         rm -rf data.$1 
 }
 
